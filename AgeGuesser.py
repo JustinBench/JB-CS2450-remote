ecc11182d715e0c523909e2da320b578ba064c0d
Justin Bench <d00188916@utahtech.edu>
2026-01-20 10:06:58 -0700

    first commit

---
+++ AgeGuesser.py
@@ -0,0 +1,34 @@
+import random
+
+def intro():
+    print("Hello! I'm going to guess your age!")
+    return
+
+def getName():
+    name = input("What's your name? ")
+    return name
+
+def guess():
+    return random.randint(15, 31)
+
+def getAnswer(age):
+    answer = input("Are you " + str(age) + " years old? (y/n) ")
+    return answer
+
+def conclusion(name, age, answer):
+    if answer == 'y':
+        print(name + ' is ' + str(age) + ' years old.')
+    else:
+        print("Rats.")
+    return
+
+def main():
+    intro()
+    name = getName()
+    age = guess()
+    answer = getAnswer(age)
+    conclusion(name, age, answer)
+
+if __name__ == '__main__':
+    main()
+
